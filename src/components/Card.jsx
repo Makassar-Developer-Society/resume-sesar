7fe5b77aa4d22dc5cf413fc0f6b75e96d32a60b0
alsanusi <malkautsars@gmail.com>
2020-09-27 17:31:44 +0800

    fix: card bold

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,41 +5,44 @@ import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
-  root: {
-    minWidth: 275,
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
+    root: {
+        minWidth: 275,
+    },
+    bullet: {
+        display: 'inline-block',
+        margin: '0 2px',
+        transform: 'scale(0.8)',
+    },
+    title: {
+        fontSize: 14,
+    },
+    pos: {
+        marginBottom: 12,
+    },
+    bold: {
+        fontWeight: 'bold'
+    }
 });
 
 export default function SimpleCard() {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  return (
-    <Card className={classes.root}>
-      <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          Makassar Dev Society
+    return (
+        <Card className={classes.root}>
+            <CardContent>
+                <Typography className={classes.title} color="textSecondary" gutterBottom>
+                    Makassar Dev Society
         </Typography>
-        <Typography variant="h5" component="h2">
-          Muhammad Alkautsar Sanusi
+                <Typography variant="h5" component="h2" className={classes.bold}>
+                    Muhammad Alkautsar Sanusi
         </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          Lucky Laki
+                <Typography className={classes.pos} color="textSecondary">
+                    Lucky Laki
         </Typography>
-        <Typography variant="body2" component="p">
-            Yang penting halal!
+                <Typography variant="body2" component="p">
+                    Yang penting halal!
         </Typography>
-      </CardContent>
-    </Card>
-  );
+            </CardContent>
+        </Card>
+    );
 }
